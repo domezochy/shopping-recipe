ee49cac6626fec54b437bddb1a8da84053c6c86f
domezochy <domezochy@gmail.com>
2022-01-30 07:20:05 -0600

    updated header recipe-list shopping-list and shopping=edit component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+    new Recipe('Atest Recipe','This is simple a test','https://www.cookipedia.co.uk/wiki/images/c/c2/Boiled_rice.jpg'),
+    new Recipe('Atest Recipe','This is simple a test','https://www.cookipedia.co.uk/wiki/images/c/c2/Boiled_rice.jpg')
+  ];
 
   constructor() { }
 
