fcf34096f8be1841bbea018991e4aa6001a54111
domezochy <domezochy@gmail.com>
2022-01-31 04:54:43 -0600

    update shared ingredient class

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,9 +1,3 @@
 export class Ingredient {
-    public name: string;
-    public amount: string;
-
-    constructor(name: string, amount: string){
-        this.name = name;
-        this.amount = amount;
-    }
+    constructor(public name: string, public amount: string){}
 }
